4198b70e6f3dbfacc931f2b3ca51a35456831592
Jordan Shatford <jordanshatford@live.com>
2022-03-11 09:05:50 -0400

    feat(filter): filter bad words from lists
    The use of this package is for games like scrabble
    and wordle. These bad words dont belong here, and
    were caught in another project.

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,20 @@
+import Filter from "bad-words"
+
 export function snakeToCamel(str: string): string {
   return str
     .toLowerCase()
     .replace(/([-_][a-z])/g, (group) => group.toUpperCase().replace('-', '').replace('_', ''));
+}
+
+export function filterBadWords(words: string[]) {
+  const badWordPlaceHolderLetter = "*"
+  const filter = new Filter({ placeHolder: badWordPlaceHolderLetter })
+  const filteredWords = words.filter(w => {
+    const cleaned = filter.clean(w)
+    const bad = cleaned.split("").every(l => l === badWordPlaceHolderLetter)
+    return !bad
+  })
+  return filteredWords
 }
 
 export function processWordsFileContent(content: string) {
@@ -9,5 +22,7 @@ export function processWordsFileContent(content: string) {
   const wordsWithoutPossesives = splitWordsList.filter((word) => {
     return !/'s$/.test(word);
   });
-  return wordsWithoutPossesives.sort();
+  const sortedFileWords = wordsWithoutPossesives.sort();
+  const filteredWords = filterBadWords(sortedFileWords)
+  return filteredWords
 }
